843f24fd9d740c3a9804fec56bfcc239f334f66d
Philip Larsson <lormiz@gmail.com>
2016-11-01 20:12:45 +0100

    Create P1_5.java

---
+++ Chapter-1-Introduction/P1_5.java
@@ -0,0 +1,11 @@
+/**
+ * Write a program that displays your name inside of a box on the screen.
+ * Do your best to approximate lines with characters such as | - +
+ */
+
+public class P1_5 {
+	public static void main(String[] args) {
+		System.out.print("------------" + "\n|   Name   |" + "\n------------");
+		
+	}
+}
