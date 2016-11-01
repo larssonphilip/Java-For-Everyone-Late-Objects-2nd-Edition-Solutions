363e5b9ed7154f9a62a8e92b48a8d1ab251511a8
Philip Larsson <lormiz@gmail.com>
2016-11-01 20:14:17 +0100

    Create P1_6.java

---
+++ Chapter-1-Introduction/P1_6.java
@@ -0,0 +1,15 @@
+/**
+ * Write your program that prints your name in large letters.
+ */
+
+public class P1_6 {
+	public static void main(String[] args) {
+		System.out.println("*     *   *****   *      *  *******      *     *   *****   *         *  ********");
+		System.out.println(" *   *   *     *  *      *  *      *     * *   *  *     *  **       **  *");
+		System.out.println("  * *    *     *  *      *  *     *      *  *  *  *     *  * *     * *  *");
+		System.out.println("   *     *     *  *      *  ******       *   * *  *******  *  *   *  *  ******");
+		System.out.println("   *     *     *  *      *  *      *     *    **  *     *  *   * *   *  *");
+		System.out.println("   *      *****    ******   *       *    *     *  *     *  *    *    *  ********");
+		
+	}
+}
