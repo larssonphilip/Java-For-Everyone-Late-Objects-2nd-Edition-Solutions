ec62a546a417da6a8cf480683ec359eea2240707
Philip Larsson <lormiz@gmail.com>
2016-11-01 20:24:54 +0100

    Create P1_7.java

---
+++ Chapter-1-Introduction/P1_7.java
@@ -0,0 +1,20 @@
+/**
+ * Write a face that is similar to (but different from) the following:
+ *   /////
+ *  +'''''+
+ * (| o o |)
+ *  |  ^  |
+ *  | '_' |
+ *  +-----+
+ */
+
+public class P1_7 {
+	public static void main(String[] args) {
+		System.out.println("  /////");
+    System.out.println(" +'''''+");
+    System.out.println("(| o o |)");
+    System.out.println(" |  ^  |");
+    System.out.println(" | '_' |");
+    System.out.println(" +-----+");
+	}
+}
