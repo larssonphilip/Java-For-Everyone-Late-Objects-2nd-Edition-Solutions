05eb3ba5a1148dfc9ac288915d968559466c7450
Philip Larsson <lormiz@gmail.com>
2016-11-01 20:28:32 +0100

    Create P1_10.java

---
+++ Chapter-1-Introduction/P1_10.java
@@ -0,0 +1,19 @@
+/**
+ * Write a program that prints an animal speaking a greeting,
+ * similar to (but different from) the following:
+ *  /\_/\    ------
+ * ( ' ' )  / Hello \
+ * (  -  ) <  Junior |
+ *  | | |   \ Coder /
+ * (__|__)    ------
+ */
+
+public class P1_10 {
+	public static void main(String[] args) {
+		System.out.println("  /\\_/\\    -------");
+		System.out.println(" ( ' ' )  / Hello \\");
+		System.out.println(" (  -  ) <  Junior |");
+		System.out.println("  | | |   \\ Coder /");
+		System.out.println(" (__|__)   -------");
+	}
+}
