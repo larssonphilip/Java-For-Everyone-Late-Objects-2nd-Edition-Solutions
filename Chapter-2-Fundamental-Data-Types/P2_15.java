d0cbb6116b3969605501081c7b4236ee1b7f7e9f
Philip Larsson <lormiz@gmail.com>
2016-12-17 11:43:55 +0100

    Update P2_15.java

--- Chapter-2-Fundamental-Data-Types/P2_15.java
+++ Chapter-2-Fundamental-Data-Types/P2_15.java
@@ -2,6 +2,6 @@ public class P2_15 {
 	public static void main(String[] args) {
 		String comb = "+--+--+--+\n|  |  |  |";
 		String bottomLine = "\n+--+--+--+";
-		System.out.println(comb + "\n" + comb + "\n" + comb + bottomLine);
+		System.out.printf("%s%n%s%n%s%s", comb, comb, comb, bottomLine);
 	}
 }
