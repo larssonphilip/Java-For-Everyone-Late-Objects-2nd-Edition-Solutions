febb0ea36e06b0c1fa51903b07a8f1a79e4a5340
Philip Larsson <lormiz@gmail.com>
2016-12-17 12:12:51 +0100

    Create P2_18.java

---
+++ Chapter-2-Fundamental-Data-Types/P2_18.java
@@ -0,0 +1,14 @@
+/**
+ * Writing large letters.
+ */
+
+public class P2_18 {
+	public static void main(String[] args) {
+		final String letterH = "*      *\n*      *\n*      *\n********\n*      *\n*      *\n*      *";
+		final String letterE = "\n\n********\n*\n*\n*****\n*\n*\n********";
+		final String letterL = "\n\n*\n*\n*\n*\n*\n*\n********";
+		final String letterO = "\n\n ******\n*      *\n*      *\n*      *\n*      *\n*      *\n ******";
+		
+		System.out.println(letterH + letterE + letterL + letterL + letterO);
+	}
+}
