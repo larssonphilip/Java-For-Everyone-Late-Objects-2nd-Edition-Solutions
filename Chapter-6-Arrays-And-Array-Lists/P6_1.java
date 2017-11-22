a1aa16f0897c9b0a2334eb058607613cfd0bbebf
Philip Larsson <lormiz@gmail.com>
2017-11-22 13:40:33 +0100

    Update P6_1.java

--- Chapter-6-Arrays-And-Array-Lists/P6_1.java
+++ Chapter-6-Arrays-And-Array-Lists/P6_1.java
@@ -8,29 +8,19 @@
 public class P6_1 {
 	public static void main(String[] args) {
 		int[] numbersArr = new int[10];
-		for(int i = 0; i < numbersArr.length; i++) {
-			numbersArr[i] = (int) (Math.random() * 100 + 1);
-		}
+		for(int i = 0; i < numbersArr.length; i++) numbersArr[i] = (int) (Math.random() * 100 + 1);
+		
 		System.out.println("The array: ");
-		for(int i = 0; i < numbersArr.length; i++) {
-			System.out.print(numbersArr[i] + "\t");
-		}
+		for(int i = 0; i < numbersArr.length; i++) System.out.print(numbersArr[i] + "\t");
+		
 		System.out.println("\n\nEvery element at an even index: ");
-		for(int i = 0; i < numbersArr.length; i++) {
-			if(i % 2 == 0)
-				System.out.print(numbersArr[i] + "\t");
-		}
+		for(int i = 0; i < numbersArr.length; i++) if(i % 2 == 0) System.out.print(numbersArr[i] + "\t");
 		
 		System.out.println("\n\nEvery even element: ");
-		for(int i = 0; i < numbersArr.length; i++) {
-			if(numbersArr[i] % 2 == 0)
-				System.out.print(numbersArr[i] + "\t");
-		}
+		for(int i = 0; i < numbersArr.length; i++) if(numbersArr[i] % 2 == 0) System.out.print(numbersArr[i] + "\t");
 		
 		System.out.println("\n\nAll elements in reverse order: ");
-		for(int i = numbersArr.length - 1; i >= 0; i--) {
-			System.out.print(numbersArr[i] + "\t");
-		}
+		for(int i = numbersArr.length - 1; i >= 0; i--) System.out.print(numbersArr[i] + "\t");
 		
 		System.out.println("\n\nOnly the first and the last element: ");
 		System.out.print(numbersArr[0] + "\t");
