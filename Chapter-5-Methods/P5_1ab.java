a6fb1eff4e0601a0b331f39792104e8737a17998
Philip Larsson <lormiz@gmail.com>
2017-11-22 12:44:37 +0100

    Update P5_1ab.java

--- Chapter-5-Methods/P5_1ab.java
+++ Chapter-5-Methods/P5_1ab.java
@@ -7,7 +7,6 @@
 import java.util.Scanner;
 
 public class P5_1 {
-	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Type three numbers, with a space separating them: ");
 		double number1= input.nextDouble();
@@ -15,20 +14,12 @@ public class P5_1 {
 		double number3 = input.nextDouble();
 		input.close();
 
-		System.out.println("The smallest number is: " + smallest(number1, number2, number3));
+		System.out.printf("The smallest number is: %.1f %n", smallest(number1, number2, number3));
 		System.out.printf("The average is: %.1f %n", average(number1, number2, number3));
 	}
 
 	public static double smallest(double x, double y, double z) {
-		if((x <= y && x <= z)) {
-			return x;
-		}
-		else if(y <= x && y <= z) {
-			return y;
-		}
-		else {
-			return z;
-		}
+		return (x <= y && x <= z) ? x : (y <= x && y <= z) ? y : z;
 	}
 
 	public static double average(double x, double y, double z) {
