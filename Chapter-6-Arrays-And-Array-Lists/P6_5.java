51c3be9d623535db27a67ec9112e8d9eab7254da
Philip Larsson <lormiz@gmail.com>
2017-11-22 13:57:27 +0100

    Update P6_5.java

--- Chapter-6-Arrays-And-Array-Lists/P6_5.java
+++ Chapter-6-Arrays-And-Array-Lists/P6_5.java
@@ -16,31 +16,24 @@ public class P6_5 {
 		System.out.print("\n\nType a index number between 0 - " + (numbers.length-1) + ": ");
 		int index = input.nextInt();
 		input.close();
-		if(index < numbers.length && index >= 0)
-			removeAtIndex(index, numbers);
-		else
-			System.out.println("There is no index with the number " + index + ".");
+		
+		if(index < numbers.length && index >= 0) removeAtIndex(index, numbers);
+		else System.out.println("There is no index with the number " + index + ".");
 	}
 
 	public static void removeAtIndex(int index, int[] inArray) {
 	        int removedNumber = inArray[index];
 	        System.arraycopy(inArray, index+1, inArray, index, inArray.length - 1 - index);
 	        System.out.print("New array: ");
-	        for(int i = 0; i < inArray.length; i++) {
-	        		System.out.print(inArray[i] + "  ");
-			}
+	        for(int i = 0; i < inArray.length; i++) System.out.print(inArray[i] + "  ");
 	        System.out.print("\n\nThe number " + removedNumber + " has been removed from the array.");
 	}
 	
 	public static void arrayAndIndex(int[] inArray) {
 		System.out.print("Array: ");
-		for(int i = 0; i < inArray.length; i++) {
-			System.out.print(inArray[i] + "  ");
-		}
+		for(int i = 0; i < inArray.length; i++) System.out.print(inArray[i] + "  ");
 		
 		System.out.print("\nIndex: ");
-		for(int i = 0; i < inArray.length; i++) {
-			System.out.print(i + "  ");
-		}
+		for(int i = 0; i < inArray.length; i++) System.out.print(i + "  ");
 	}
 }
