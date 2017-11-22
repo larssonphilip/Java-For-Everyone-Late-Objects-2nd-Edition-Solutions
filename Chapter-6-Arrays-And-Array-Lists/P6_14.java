c57065fe57186a118241fc4c2e57a3ccf8cb6bb0
Philip Larsson <lormiz@gmail.com>
2017-11-22 14:00:04 +0100

    Update P6_14.java

--- Chapter-6-Arrays-And-Array-Lists/P6_14.java
+++ Chapter-6-Arrays-And-Array-Lists/P6_14.java
@@ -12,23 +12,16 @@ public class P6_14 {
 	
 	public static int[] sequenceGenerator(int arraySize) {
 		int[] inArray = new int[arraySize];
-		
-		for(int i = 0; i < arraySize; i++) {
-			inArray[i] = (int) (Math.random() * 100);
-		}
+		for(int i = 0; i < arraySize; i++) inArray[i] = (int) (Math.random() * 100);
 		return inArray;
 	}
 	
 	public static void printAndSortArray(int[] inArray) {
 		System.out.println("Unsorted array: ");
-		for(int i = 0; i < inArray.length; i++) {
-			System.out.print(inArray[i] + "  ");
-		}
+		for(int i = 0; i < inArray.length; i++) System.out.print(inArray[i] + "  ");
 		
 		Arrays.sort(inArray);
 		System.out.println("\n\nSorted array: ");
-		for(int i = 0; i < inArray.length; i++) {
-			System.out.print(inArray[i] + "  ");
-		}
+		for(int i = 0; i < inArray.length; i++) System.out.print(inArray[i] + "  ");
 	}
 }
